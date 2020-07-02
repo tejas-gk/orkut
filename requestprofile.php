b629589b2c175f9287d07f74d21664e0ad4d3e4b
tejas-gk <66897829+tejas-gk@users.noreply.github.com>
2020-07-02 22:07:18 +0530

    added sidebar

--- requestprofile.php
+++ requestprofile.php
@@ -1,7 +1,23 @@
 <?php
 include "config.php";
-echo "<h1>hi</h1>";
+include "navbar.php";
+
+$otpt="SELECT pname,pemail FROM orkut";
 
  ?>
  <br>
-<a href="logout.php">logout</a>
+<style media="screen">
+
+</style>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style/leftlay.css">
+  </head>
+  <body>
+    <?php   include "leftlay.php";?>
+  </body>
+</html>
+<a href="logout.php" class="logout">logout</a>
