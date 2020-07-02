b3985b057198260871529a6e2205652adae27421
tejas-gk <66897829+tejas-gk@users.noreply.github.com>
2020-07-02 22:11:27 +0530

    profile
    this should be in another folder called profile

---
+++ proflie.php
@@ -0,0 +1,11 @@
+<?php include "../config.php"; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php include "../leftlay.php"; ?>
+  </body>
+</html>
