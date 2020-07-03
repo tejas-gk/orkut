05538e4ed8bccf6be413791462120cc6d91fc907
tejas-gk <66897829+tejas-gk@users.noreply.github.com>
2020-07-03 21:05:50 +0530

    added sidebar

--- requestprofile.php
+++ requestprofile.php
@@ -17,7 +17,19 @@ $otpt="SELECT pname,pemail FROM orkut";
     <link rel="stylesheet" href="style/leftlay.css">
   </head>
   <body>
-    <?php   include "leftlay.php";?>
+    <div class="container leftrow" style="">
+        <div class="row">
+          <div class="col-12" >
+            <ul class="list-group">
+                <li class="list-group-item active"><a href="..\requestprofile.php">feed</a></li>
+                <li class="list-group-item"><a href="profile\proflie.php">profile</a></li>
+                <li class="list-group-item">collection</li>
+
+
+            </ul>
+          </div>
+        </div>
+    </div>
   </body>
 </html>
 <a href="logout.php" class="logout">logout</a>
