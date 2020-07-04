ed140e1d4255c332244f90f2dd2bc556f61ec629
tejas-gk <66897829+tejas-gk@users.noreply.github.com>
2020-07-04 18:36:17 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,47 +1,26 @@
-<div class="container m-5">
-  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="transition-delay: 2s; background-color:black;">
-<a class="navbar-brand text-light text-uppercase" href="#" >logo</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link text-light text-uppercase" href="#">
-        <div class="fsize">home</div>
-        <span class="sr-only">(current)</span></a>
-    </li>
-    <!-- ================================================================= -->
-    <li class="nav-item">
-      <a class="nav-link text-light text-uppercase" href="#"><div class="fsize">chat</div></a>
-    </li>
-    <!-- ================================================================== -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle text-light text-uppercase" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <div class="fsize">more</div>
-      </a>
-      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="#"><div class="fsize">action</div></a>
-        <a class="dropdown-item" href="#"><div class="fsize">anither action</div></a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#"><div class="fsize">something else here</div></a>
-      </div>
-    </li>
-<!-- ================================================================= -->
-<div class="mx-auto">
-<li class="nav-item">
-<a class="nav-link text-light text-uppercase" href="profile.php"><div class="fsize">profile</div></a>
-</li>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<!-- Sidebar -->
+<div class="w3-sidebar w3-bar-block w3-border-right" style="display:none" id="mySidebar">
+<button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>
+<a href="requestprofile.php" class="w3-bar-item w3-button">feed</a>
+<a href="profile/postbody.php" class="w3-bar-item w3-button">profile</a>
+<a href="#" class="w3-bar-item w3-button">Link 3</a>
+</div>
+<!-- ================ -->
+<!-- Page Content -->
+<div class="w3-teal">
+<button class="w3-button w3-teal w3-xlarge" onclick="w3_open()">☰</button>
+<div class="w3-container">
+<h1>My Page</h1>
+</div>
 </div>
 
-  </ul>
-  <!-- ===================================================================================== -->
-  <form class="form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</div>
+<script>
+function w3_open() {
+document.getElementById("mySidebar").style.display = "block";
+}
 
-</nav>
-</div>
+function w3_close() {
+document.getElementById("mySidebar").style.display = "none";
+}
+</script>
